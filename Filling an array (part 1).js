029be557f524d9752a0f62889a5c1508fed23efe
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-10-08 23:11:48 -0400

    Add files via upload

---
+++ Filling an array (part 1).js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
+
+    function arr(N){
+        let newArray = []
+        for (let i = 0; i < N; i++){
+            newArray.push(i)
+        }  return newArray
+    }
