fd3748898393af154a037fd6408f01deea9fd20d
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-10-13 21:06:34 -0400

    Add files via upload

---
+++ 101 Dalmatians - squash the bugs, not the dogs!.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/56f6919a6b88de18ff000b36/train/javascript
+
+    function howManyDalmatians(numer) {
+        return numer <= 10 ? "Hardly any"
+            : numer <= 50 ? "More than a handful!"
+                : numer === 101 ? "101 DALMATIANS!!!"
+                    : "Woah that's a lot of dogs!"
+    }
