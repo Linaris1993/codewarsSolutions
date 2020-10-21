65bbc5a44c753a88258f27c138546757ae6042dd
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-10-20 20:35:40 -0400

    Update 101 Dalmatians - squash the bugs, not the dogs!.js

--- 101 Dalmatians - squash the bugs, not the dogs!.js
+++ 101 Dalmatians - squash the bugs, not the dogs!.js
@@ -1,8 +1,8 @@
 https://www.codewars.com/kata/56f6919a6b88de18ff000b36/train/javascript
 
     function howManyDalmatians(numer) {
-        return numer <= 10 ? "Hardly any"
-            : numer <= 50 ? "More than a handful!"
-                : numer === 101 ? "101 DALMATIANS!!!"
-                    : "Woah that's a lot of dogs!"
+      return numer <= 10 ? "Hardly any"
+     : numer <= 50 ? "More than a handful!"
+     : numer === 101 ? "101 DALMATIANS!!!"
+     : "Woah that's a lot of dogs!"
     }
