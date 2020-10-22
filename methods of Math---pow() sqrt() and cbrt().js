02a147f98ba4fc816fd0dd6cdfdaa97823d74333
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-10-22 00:01:55 -0400

    Update methods of Math---pow() sqrt() and cbrt().js

--- methods of Math---pow() sqrt() and cbrt().js
+++ methods of Math---pow() sqrt() and cbrt().js
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/5733f948d780e27df6000e33/train/javascript
 function cutCube(volume,n){
-    return (Math.round(Math.cbrt(volume)) ** 3 === volume) && (Math.round(Math.cbrt(n)) ** 3 === n) && (volume / n) % 1 === 0
+return (Math.round(Math.cbrt(volume)) ** 3 === volume) && (Math.round(Math.cbrt(n)) ** 3 === n) && (volume / n) % 1 === 0
 }
