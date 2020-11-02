3bbc7bc0a623f4cda105515520852aea1ecc8ece
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-11-02 18:32:20 -0500

    Add files via upload

---
+++ Array Mash.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/582642b1083e12521f0000da/train/javascript
+
+    function arrayMash (array1, array2) {
+        let res = [];
+        for (let i = 0; i < array1.length; i++){
+            res.push(array1[i])
+            res.push(array2[i])
+        }
+        return res
+    }
