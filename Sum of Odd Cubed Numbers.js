b4fbc0af96f12b8519dc3bbe7d6351d50a9f20d2
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-11-03 19:47:12 -0500

    Add files via upload

---
+++ Sum of Odd Cubed Numbers.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/580dda86c40fa6c45f00028a/train/javascript
+
+    function cubeOdd(arr) {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++){
+            if (typeof arr[i] !== 'number') { return undefined
+            } else if (typeof arr[i] === 'number' && arr[i] % 2 !== 0){
+                arr[i] = arr[i]**3
+                sum += arr[i]
+            }
+        } return sum
+    }
