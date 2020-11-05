9cb20132a1716d85b8a908ac3dd1618475ce688e
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-11-05 16:20:20 -0500

    Add files via upload

---
+++ Simple consecutive pairs.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/5a3e1319b6486ac96f000049/train/javascript
+
+    function pairs(ar){
+        let count = 0;
+        let res = [];
+        for (let i = 0; i < ar.length; i+=2){
+            if (ar[i] + 1 === ar[i+1] || ar[i] - 1 === ar[i+1]){
+                res.push(ar[i])
+            }
+        } return res.length
+    }
