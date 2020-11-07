f664e610d9bc6b41d218839959553d87406c9bf2
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2020-11-06 19:12:26 -0500

    Add files via upload

---
+++ Plus - minus - plus - plus - ... - Count.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/5bbb8887484fcd36fb0020ca/train/javascript
+
+    function catchSignChange(arr){
+        let count = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] < 0 &&  arr[i+1] >= 0) count++
+            if (arr[i] >= 0 && arr[i+1] < 0) count++
+        } return count
+    }
