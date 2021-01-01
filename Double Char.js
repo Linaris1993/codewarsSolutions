53b3c83e14f06df003e5b8c2894f9882419fd06f
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2021-01-01 13:30:09 -0500

    Add files via upload

---
+++ Double Char.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
+
+    function doubleChar(str) {
+        let res = ""
+        for (let el of str){
+            res += el + el
+        } return res
+    }
