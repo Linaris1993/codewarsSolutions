403eacadc9f6ce0430489e5637b1663b7af6d637
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2021-01-11 14:42:49 -0500

    Add files via upload

---
+++ Reversed Strings.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+    function solution(str){
+        let n = ""
+        for (let i = str.length-1; i>=0; i--){
+            n += str[i]
+        } return n
+    }
