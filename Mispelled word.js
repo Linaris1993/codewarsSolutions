7ac9dd47d3758ca76d21e9e09315c26ac2302585
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2021-02-15 19:57:42 -0500

    Add files via upload

---
+++ Mispelled word.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/5892595f190ca40ad0000095/train/javascript
+
+    var mispelled = function(word1, word2){
+        if (Math.abs(word1.length - word2.length) > 1) return false
+        let count = 0;
+        for (let i = 0; i < word1.length; i++){
+            if (word1[i] !== word2[i]){
+                count++
+            }
+        } return count === 1 || word1.includes(word2) || word2.includes(word1)
+    }
