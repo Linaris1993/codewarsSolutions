429796916f3b6ad1b28772cf018ba72d0dcfa914
Linaris1993 <69872140+Linaris1993@users.noreply.github.com>
2025-07-22 13:12:08 -0700

    Update Abbreviate a Two Word Name.js

--- Abbreviate a Two Word Name.js
+++ Abbreviate a Two Word Name.js
@@ -9,3 +9,24 @@ https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
         }
         return name[0].toUpperCase() + "." + `${n}`.toUpperCase()
     }
+
+//another solution
+function abbrevName(name){
+ let str2 = ''
+ let res = name.split(' ').map((el, i) => {
+    if (i === 0) {
+    return el[0].toUpperCase()
+    }
+    else {
+     return el[0].toUpperCase()
+    }
+  }
+ )
+res.join('')
+for (let i = 0; i < res.length; i++) {
+  if (i < res.length) {
+  str2 += res[i] + '.'
+}
+}
+return str2.slice(0, -1)
+}
